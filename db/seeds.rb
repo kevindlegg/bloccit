a8ccf07fec59c3aa235737210412b023097a1be8
K Legg <kevindlegg@gmail.com>
2018-07-17 16:39:27 -0500

    assignment 11 more CRUD: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,7 @@ require 'random_data'
 
 # Create Posts
 50.times do
-# #1
   Post.create!(
-# #2
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
@@ -19,14 +17,22 @@ end
 posts = Post.all
 
 # Create Comments
-# #3
 100.times do
   Comment.create!(
-# #4
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
+
+# Create Questions
+50.times do
+    Question.create!(
+      title:  RandomData.random_sentence,
+      body:   RandomData.random_paragraph,
+      resolved: false
+    )
+  end
+  questions = Question.all
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
