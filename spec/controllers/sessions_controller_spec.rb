a3e2361bdcdf25ca2bea0dd7f7175bee8ef9fde5
K Legg <kevindlegg@gmail.com>
2018-08-01 20:53:26 -0500

    checkpoint 16 rails authentication signing in

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe SessionsController, type: :controller do
 
   describe "POST sessions" do
     it "returns http success" do
-      post :create, params: { sesssion: { email: my_user.email } }
+      post :create, params: { session: { email: my_user.email } }
       expect(response).to have_http_status(:success)
     end
 
@@ -22,22 +22,22 @@ RSpec.describe SessionsController, type: :controller do
     end
 
     it "does not add a user id to session due to missing password" do
-      post :create, params: { sesssion: { email: my_user.email } }
+      post :create, params: { session: { email: my_user.email } }
       expect(session[:user_id]).to be_nil
     end
 
     it "flashes #error with bad email address" do
-      post :create, params: { sesssion: { email: "does not exist" } }
+      post :create, params: { session: { email: "does not exist" } }
       expect(flash.now[:alert]).to be_present 
     end
 
     it "renders #new with bad email address" do
-      post :create, params: { sesssion: { email: "does not exist" } }
+      post :create, params: { session: { email: "does not exist" } }
       expect(response).to render_template :new
     end
 
     it "redirects to the root view" do
-      post :create, params: { sesssion: { email: my_user.email, password: my_user.password } }
+      post :create, params: { session: { email: my_user.email, password: my_user.password } }
       expect(response).to redirect_to(root_path)
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -58,5 +58,6 @@ RSpec.describe UsersController, type: :controller do
       post :create, params: { user: new_user_attributes }
       expect(session[:user_id]).to eq assigns(:user).id
     end
+    
   end
 end
