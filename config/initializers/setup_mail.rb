e38f517d7591e0a74e6ad0f425a3b8b96b85234d
K Legg <kevindlegg@gmail.com>
2018-08-24 13:40:16 -0500

    resolving sendgrid issues

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ if Rails.env.development? || Rails.env.production?
     port:           '2525',
     authentication: :plain,
     user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
+    api_key:        ENV['SENDGRID_API_KEY'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
